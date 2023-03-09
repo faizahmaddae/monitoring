6062431c036bb893afa7dc5d3d1b529b6f600122
faizahmaddae <faizahmad.daiee@gmail.com>
2023-03-09 07:54:27 -0800

    new update core

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+header('Access-Control-Allow-Origin: *');
+
 use App\Http\Controllers\incomingApi\AuthController;
 use App\Http\Controllers\incomingApi\CategoryController;
 use App\Http\Controllers\incomingApi\ClientController;
